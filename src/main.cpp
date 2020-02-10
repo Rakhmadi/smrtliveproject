38e24e15b22d73481972253df055b0fb831b4355
rakhmadi D <rakhmadiwalker@gmail.com>
2020-02-10 20:08:05 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-
+//////////HEADER////////////
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
@@ -9,7 +9,7 @@
 #include <Adafruit_Sensor.h>
 #include <stdio.h>
 
-///////////////////////////////////
+/////////////DEFINE///////////////
 #define DRD_TIMEOUT 2.0
 #define DRD_ADDRESS 0x00
 #define PORT_REST_HTTP 80
@@ -17,21 +17,23 @@
 #define SYS_DHT_TYPE DHT11
 //////////////////////////////////
 DHT dht;
-
 WiFiServer server(80);
 byte led = 16;
 byte ds = 2;
 ESP8266WebServer http_rest_server(PORT_REST_HTTP);
+//FUNCTION DHT11
 void get_l(){
     if (false)
-        http_rest_server.send(204);
+        http_rest_server.send(204); // :)
     else {
-      long rsii= WiFi.RSSI();
-      float tmp =dht.getTemperature();
-      float hmdt=dht.getHumidity();
+       long rsii= WiFi.RSSI();
+       //MENGAMBIL TEMPERATUR& HUMIDTY
+       float tmp =dht.getTemperature();
+       float hmdt=dht.getHumidity();
+
        StaticJsonBuffer<200> jsonBuffer;
-    JsonObject& jsonObj = jsonBuffer.createObject();
-    char JSONmessageBuffer[200];
+       JsonObject& jsonObj = jsonBuffer.createObject();
+       char JSONmessageBuffer[200];
        jsonObj["id"] = 1;
        jsonObj["temperatur"] =tmp;
        jsonObj["kelembapan"] =hmdt;
@@ -44,6 +46,7 @@ void get_l(){
        http_rest_server.send(200, "application/json",JSONmessageBuffer );
     }
 }
+//FUNCTION RELAY
 void relay(){
       String condition =http_rest_server.arg("condition");
      if (condition == "on") {
@@ -51,20 +54,26 @@ void relay(){
      } else if (condition == "off") {
         digitalWrite(led,0x000000000001);
      } else {
+          
+     }
        StaticJsonBuffer<200> jsonBuffer;
-    JsonObject& jsonObj = jsonBuffer.createObject();
-    char dd[200];
-       jsonObj["msg"] = "value_error_msf_false";
-       jsonObj.prettyPrintTo(dd, sizeof(daddr_t));
+       JsonObject& jsonObj = jsonBuffer.createObject();
+       char msbubb[200];
+       jsonObj["condition"] = condition;
+       jsonObj.prettyPrintTo(msbubb, sizeof(msbubb));
        http_rest_server.sendHeader("Access-Control-Allow-Methods", "*");
        http_rest_server.sendHeader("Access-Control-Allow-Origin", "*");
        http_rest_server.sendHeader("Access-Control-Allow-Headers", "*");
-      http_rest_server.send(400, "application/json",dd );
-     }
-    StaticJsonBuffer<200> jsonBuffer;
-    JsonObject& jsonObj = jsonBuffer.createObject();
-    char msbubb[200];
-       jsonObj["condition"] = condition;
+       http_rest_server.send(200, "application/json",msbubb );
+}
+//FUNCTION HOME
+void qwer(){
+       char rt=digitalRead(led);
+       StaticJsonBuffer<200> jsonBuffer;
+       JsonObject& jsonObj = jsonBuffer.createObject();
+       char msbubb[200];
+       jsonObj["status"] = WiFi.status();
+       jsonObj["rely"] =rt;
        jsonObj.prettyPrintTo(msbubb, sizeof(msbubb));
        http_rest_server.sendHeader("Access-Control-Allow-Methods", "*");
        http_rest_server.sendHeader("Access-Control-Allow-Origin", "*");
@@ -75,27 +84,35 @@ void relay(){
 void  sys_route_http(){
        http_rest_server.on("/dht", HTTP_GET, get_l);
        http_rest_server.on("/relay",relay);
+       http_rest_server.on("/",qwer);
+      
 }
-////////////////////////////////////////////
-
+      ////////////////////////////////////////////
 void setup() {
-  WiFiManager wifimanager;
-  wifimanager.autoConnect("IoT_CoNfT_","nulllablestc");
-  pinMode(led, OUTPUT);
-   DoubleResetDetect drd(DRD_TIMEOUT, DRD_ADDRESS);
-   if (drd.detect())
+       ////////////////////Setup Wifimanager
+       WiFiManager wifimanager;
+       wifimanager.autoConnect("IoT_Dinamika","qwerty123");
+       pinMode(led, OUTPUT); //relay mode output
+       ///// Button reset setup
+       DoubleResetDetect drd(DRD_TIMEOUT, DRD_ADDRESS);
+   if (drd.detect()) 
     {
-        WiFi.disconnect();
+        WiFi.disconnect();// Disconect wifi goto back to wifi configuration WiFimanager
         Serial.print("");
     }else{
 
     }
+  //mendefinisikan routing
   sys_route_http();
+  //webserver begin
   http_rest_server.begin();
+  //DHTSETUP
   dht.setup(SYS_DHT_OUT_PIN);
+  //SERVER BEGIN
   server.begin();
 }
 void loop() {
+  //handeled web server client
   http_rest_server.handleClient();
 }
 ///////////////////////////////////////////////
